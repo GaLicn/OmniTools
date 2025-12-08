0a59116ce1e3248e0822b7c673c81a695eda92bc
GaLicn <133291877+GaLicn@users.noreply.github.com>
2025-12-08 19:30:30 +0800

    init commit

---
+++ src/main/java/com/omnitools/omniTools/omniTools.java
@@ -0,0 +1,20 @@
+package com.omnitools.omniTools;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(omniTools.MODID)
+public class omniTools {
+    public static final String MODID = "omnitools";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public omniTools(IEventBus modEventBus, ModContainer modContainer) {
+        LOGGER.info("OmniTools mod is loading...");
+    }
+}
