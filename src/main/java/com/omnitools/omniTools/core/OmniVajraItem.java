3c2f596cd2a5ee31c08fc698878c75cb7f1000d5
GaLicn <133291877+GaLicn@users.noreply.github.com>
2025-12-19 12:55:02 +0800

    useOn不切换状态

--- src/main/java/com/omnitools/omniTools/core/OmniVajraItem.java
+++ src/main/java/com/omnitools/omniTools/core/OmniVajraItem.java
@@ -164,11 +164,6 @@ public class OmniVajraItem extends Item {
         var stack = context.getItemInHand();
         var player = context.getPlayer();
 
-        if (player != null && player.isShiftKeyDown()) {
-            toggleAutoPickup(stack, level, player);
-            return InteractionResult.sidedSuccess(level.isClientSide);
-        }
-
         BlockState modifiedState = null;
         SoundEvent sound = null;
         int levelEventId = -1;
